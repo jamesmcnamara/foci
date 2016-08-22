c1101f3f472741d1bf7c234b70b4052014c54e50
jamesmcnamara <jamesscottmcnamara@gmail.com>
2016-08-22 19:45:53 -0400

    added edge case protection for single element paths

--- src/compiler/compile.js
+++ src/compiler/compile.js
@@ -5,6 +5,7 @@ import compose from '../lens-consumers/compose.js'
 
 export default (lens) => do {
     if (typeof(lens) === 'string') {
+        // concat will turn an object into a singleton array
         compose(...Array.prototype.concat(parse(lens)).map(term => do {
             if (term.type == 'attr')
                 attr(term.value)
